b75b3b495d3f0816371066fa26835d6fd84e63e1
Jay <paxformatus@gmail.com>
2018-06-05 14:16:03 -0400

    Working commit 2

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -80,6 +80,33 @@ function cycle() {
         q6();
     }
     else if (totalQuestions === 6) {
+        q7();
+    }
+    else if (totalQuestions === 7) {
+        q8();
+    }
+    else if (totalQuestions === 8) {
+        q9();
+    }
+    else if (totalQuestions === 9) {
+        q10();
+    }
+    else if (totalQuestions === 10) {
+        q11();
+    }
+    else if (totalQuestions === 11) {
+        q12();
+    }
+    else if (totalQuestions === 12) {
+        q13();
+    }
+    else if (totalQuestions === 13) {
+        q14();
+    }
+    else if (totalQuestions === 14) {
+        q15();
+    }
+    else if (totalQuestions === 15) {
         endGame();
     }
 
@@ -250,6 +277,195 @@ function q6(){
         "</label><br>" +
         "<label>" +
         "<input type='radio' name='choice' value='1'> Milhouse" +
+        "</label><br>" +
+    "</div>" +
+    "</div>" 
+    )
+}
+function q7(){
+    $('#questions').html(
+        "<br>" +
+        "<div class='col col-xs-12 col-lg-12'>" +
+        "<h3>Who shot Mr. Burns?</h3>" +
+        "<label>" +
+        "<input type='radio' name='choice' value='0'> Maggie" +
+        "</label><br>" +
+        "<label>" +
+        "<input type='radio' name='choice' value='1'> Bart" +
+        "</label><br>" +
+        "<label>" +
+        "<input type='radio' name='choice' value='1'> Smithers" +
+        "</label><br>" +
+        "<label>" +
+        "<input type='radio' name='choice' value='1'> Snake" +
+        "</label><br>" +
+    "</div>" +
+    "</div>" 
+    )
+}
+function q8(){
+    $('#questions').html(
+        "<br>" +
+        "<div class='col col-xs-12 col-lg-12'>" +
+        "<h3>What was Maggie Simpsons first word?</h3>" +
+        "<label>" +
+        "<input type='radio' name='choice' value='1'> Mama" +
+        "</label><br>" +
+        "<label>" +
+        "<input type='radio' name='choice' value='0'> Daddy" +
+        "</label><br>" +
+        "<label>" +
+        "<input type='radio' name='choice' value='1'> D'oh!" +
+        "</label><br>" +
+        "<label>" +
+        "<input type='radio' name='choice' value='1'> Liza" +
+        "</label><br>" +
+    "</div>" +
+    "</div>" 
+    )
+}
+function q9(){
+    $('#questions').html(
+        "<br>" +
+        "<div class='col col-xs-12 col-lg-12'>" +
+        "<h3>How many points does Bart's hair have?</h3>" +
+        "<label>" +
+        "<input type='radio' name='choice' value='1'> Eleven" +
+        "</label><br>" +
+        "<label>" +
+        "<input type='radio' name='choice' value='1'> Six" +
+        "</label><br>" +
+        "<label>" +
+        "<input type='radio' name='choice' value='1'> Twenty Three" +
+        "</label><br>" +
+        "<label>" +
+        "<input type='radio' name='choice' value='0'> Nine" +
+        "</label><br>" +
+    "</div>" +
+    "</div>" 
+    )
+}
+function q10(){
+    $('#questions').html(
+        "<br>" +
+        "<div class='col col-xs-12 col-lg-12'>" +
+        "<h3>What is Comic Book Guy's real name?</h3>" +
+        "<label>" +
+        "<input type='radio' name='choice' value='0'> Jeff Albertson" +
+        "</label><br>" +
+        "<label>" +
+        "<input type='radio' name='choice' value='1'> Marv Springsteen" +
+        "</label><br>" +
+        "<label>" +
+        "<input type='radio' name='choice' value='1'> Jack Black" +
+        "</label><br>" +
+        "<label>" +
+        "<input type='radio' name='choice' value='1'> John Winters" +
+        "</label><br>" +
+    "</div>" +
+    "</div>" 
+    )
+}
+function q11(){
+    $('#questions').html(
+        "<br>" +
+        "<div class='col col-xs-12 col-lg-12'>" +
+        "<h3>Who co-wrote the song Do The Bartman?</h3>" +
+        "<label>" +
+        "<input type='radio' name='choice' value='1'> Mike Perry" +
+        "</label><br>" +
+        "<label>" +
+        "<input type='radio' name='choice' value='1'> John Williams" +
+        "</label><br>" +
+        "<label>" +
+        "<input type='radio' name='choice' value='0'> Michael Jackson" +
+        "</label><br>" +
+        "<label>" +
+        "<input type='radio' name='choice' value='1'> Matt Groenig" +
+        "</label><br>" +
+    "</div>" +
+    "</div>" 
+    )
+}
+function q12(){
+    $('#questions').html(
+        "<br>" +
+        "<div class='col col-xs-12 col-lg-12'>" +
+        "<h3>How many children do Cleetus and Brandine have?</h3>" +
+        "<label>" +
+        "<input type='radio' name='choice' value='1'> 23" +
+        "</label><br>" +
+        "<label>" +
+        "<input type='radio' name='choice' value='1'> 12" +
+        "</label><br>" +
+        "<label>" +
+        "<input type='radio' name='choice' value='0'> 44" +
+        "</label><br>" +
+        "<label>" +
+        "<input type='radio' name='choice' value='1'> 6" +
+        "</label><br>" +
+    "</div>" +
+    "</div>" 
+    )
+}
+function q13(){
+    $('#questions').html(
+        "<br>" +
+        "<div class='col col-xs-12 col-lg-12'>" +
+        "<h3>Which character has dialog in every episode?</h3>" +
+        "<label>" +
+        "<input type='radio' name='choice' value='1'> Marge" +
+        "</label><br>" +
+        "<label>" +
+        "<input type='radio' name='choice' value='0'> Homer" +
+        "</label><br>" +
+        "<label>" +
+        "<input type='radio' name='choice' value='1'> Apu" +
+        "</label><br>" +
+        "<label>" +
+        "<input type='radio' name='choice' value='1'> Bart" +
+        "</label><br>" +
+    "</div>" +
+    "</div>" 
+    )
+}
+function q14(){
+    $('#questions').html(
+        "<br>" +
+        "<div class='col col-xs-12 col-lg-12'>" +
+        "<h3>Who was the first Beatle to appear on the show?</h3>" +
+        "<label>" +
+        "<input type='radio' name='choice' value='0'> Ringo" +
+        "</label><br>" +
+        "<label>" +
+        "<input type='radio' name='choice' value='1'> Paul" +
+        "</label><br>" +
+        "<label>" +
+        "<input type='radio' name='choice' value='1'> John" +
+        "</label><br>" +
+        "<label>" +
+        "<input type='radio' name='choice' value='1'> George" +
+        "</label><br>" +
+    "</div>" +
+    "</div>" 
+    )
+}
+function q15(){
+    $('#questions').html(
+        "<br>" +
+        "<div class='col col-xs-12 col-lg-12'>" +
+        "<h3>How many episodes are there currently?</h3>" +
+        "<label>" +
+        "<input type='radio' name='choice' value='1'> 634" +
+        "</label><br>" +
+        "<label>" +
+        "<input type='radio' name='choice' value='1'> 754" +
+        "</label><br>" +
+        "<label>" +
+        "<input type='radio' name='choice' value='0'> 574" +
+        "</label><br>" +
+        "<label>" +
+        "<input type='radio' name='choice' value='1'> 475" +
         "</label><br>" +
     "</div>" +
     "</div>" 
